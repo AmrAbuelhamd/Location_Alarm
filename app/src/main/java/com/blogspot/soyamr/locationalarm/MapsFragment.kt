3469df7cd9d72626340e78a57d42e4e0d03a67c8
Amr Mokhamed <amr.abuelhamd@gmail.com>
2021-04-09 00:37:00 +0700

    created the design of all fragments added viewbinding dependencies

--- app/src/main/java/com/blogspot/soyamr/locationalarm/MapsFragment.kt
+++ app/src/main/java/com/blogspot/soyamr/locationalarm/MapsFragment.kt
@@ -1,20 +1,20 @@
 package com.blogspot.soyamr.locationalarm
 
-import androidx.fragment.app.Fragment
-
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import by.kirich1409.viewbindingdelegate.viewBinding
+import com.blogspot.soyamr.locationalarm.databinding.FragmentMapsBinding
 import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import kotlinx.android.synthetic.main.fragment_maps.*
 
-class MapsFragment : Fragment() {
+class MapsFragment : Fragment(R.layout.fragment_maps) {
+    private val binding: FragmentMapsBinding by viewBinding()
 
     private val callback = OnMapReadyCallback { googleMap ->
         /**
@@ -31,17 +31,12 @@ class MapsFragment : Fragment() {
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_maps, container, false)
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
         mapFragment?.getMapAsync(callback)
+        binding.selectButton.setOnClickListener {
+            findNavController().navigate(R.id.action_mapsFragment_to_trackingFragment)
+        }
     }
 }
