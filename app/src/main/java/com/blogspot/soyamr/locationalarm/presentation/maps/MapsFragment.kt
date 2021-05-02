1ae78d0258ff4070871c3719223cdfea4b082fb3
Amr Mokhamed <amr.abuelhamd@gmail.com>
2021-05-02 18:16:57 +0700

    Optimised code Took all Location related code in separate class. Removed redundant code. Removed Log messages. Showed proper Toast messages.

--- app/src/main/java/com/blogspot/soyamr/locationalarm/presentation/maps/MapsFragment.kt
+++ app/src/main/java/com/blogspot/soyamr/locationalarm/presentation/maps/MapsFragment.kt
@@ -1,33 +1,23 @@
 package com.blogspot.soyamr.locationalarm.presentation.maps
 
-import android.Manifest
-import android.annotation.SuppressLint
 import android.app.Activity.RESULT_OK
-import android.content.IntentSender
-import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
-import android.os.Looper
 import android.view.View
 import android.widget.Toast
-import androidx.activity.result.IntentSenderRequest
 import androidx.activity.result.contract.ActivityResultContracts
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.blogspot.soyamr.locationalarm.R
 import com.blogspot.soyamr.locationalarm.databinding.FragmentMapsBinding
-import com.google.android.gms.common.api.ResolvableApiException
-import com.google.android.gms.location.*
+import com.blogspot.soyamr.locationalarm.presentation.utils.MyLocationManger
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
-import com.google.android.gms.tasks.Task
 
 
 class MapsFragment : Fragment(R.layout.fragment_maps) {
@@ -39,20 +29,9 @@ class MapsFragment : Fragment(R.layout.fragment_maps) {
 
     private lateinit var googleMap: GoogleMap
 
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
 
-    private var locationCallback: LocationCallback = object : LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult?) {
-            locationResult ?: return
-            for (location in locationResult.locations) {
-                println("heeere")
-                if (location != null) {
-                    showUserCurrentLocation(location)
-//                    stopLocationUpdates()
-                    break
-                }
-            }
-        }
+    private val locationManger: MyLocationManger by lazy {
+        MyLocationManger(requireContext(), showUserCurrentLocation)
     }
 
     private val mapFragment by lazy {
@@ -62,80 +41,24 @@ class MapsFragment : Fragment(R.layout.fragment_maps) {
     private val mapReadyCallback = OnMapReadyCallback { googleMap ->
         this.googleMap = googleMap
         try {
-            fusedLocationClient.lastLocation
-                .addOnSuccessListener { location: Location? ->
-                    if (location == null) {
-                        requestLocationUpdates()
-                    } else {
-                        showUserCurrentLocation(location)
-                    }
-                }
+            locationManger.getLastLocation(
+                resolutionForResult,
+                requestPermissionLauncher
+            )
         } catch (e: SecurityException) {
             showMessage("something went wrong, couldn't get your location")
         }
-    }
 
-    @SuppressLint("MissingPermission")
-    private fun requestLocationUpdates() {
-        val builder = LocationSettingsRequest.Builder()
-            .addLocationRequest(createLocationRequest())
-        val client: SettingsClient = LocationServices.getSettingsClient(requireActivity())
-        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
-        task.addOnSuccessListener { locationSettingsResponse ->
-            startLocationUpdates()
-        }
-
-        task.addOnFailureListener { exception ->
-            if (exception is ResolvableApiException) {
-                // Location settings are not satisfied, but this can be fixed
-                // by showing the user a dialog.
-                try {
-                    // Show the dialog by calling startResolutionForResult(),
-                    // and check the result in onActivityResult().
-//                    exception.startResolutionForResult(requireActivity(), REQUEST_CHECK_SETTINGS)
-                    val intentSenderRequest =
-                        IntentSenderRequest.Builder(exception.resolution).build()
-                    resolutionForResult.launch(intentSenderRequest)
-                } catch (sendEx: IntentSender.SendIntentException) {
-                    // Ignore the error.
-                }
-            }
-        }
-    }
-
-    private fun startLocationUpdates() {
-        if (ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) != PackageManager.PERMISSION_GRANTED
-        ) {
-            requestPermissionLauncher.launch(
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
-            return
-        }
-        fusedLocationClient.requestLocationUpdates(
-            createLocationRequest(),
-            locationCallback,
-            Looper.getMainLooper()
-        )
     }
 
     private val resolutionForResult =
         registerForActivityResult(ActivityResultContracts.StartIntentSenderForResult()) { activityResult ->
             if (activityResult.resultCode == RESULT_OK)
-                startLocationUpdates()
+                locationManger.startLocationUpdates(requestPermissionLauncher)
+            else {
+                showMessage("we can't determine your location")
+            }
         }
-
-    private fun showUserCurrentLocation(location: Location) {
-        val sydney = LatLng(location.latitude, location.longitude)
-        googleMap.addMarker(MarkerOptions().position(sydney))
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
-        googleMap.uiSettings.isZoomControlsEnabled = true;
-    }
 
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission()
@@ -150,29 +73,24 @@ class MapsFragment : Fragment(R.layout.fragment_maps) {
         }
     }
 
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        setUpListeners()
-
-        when (PackageManager.PERMISSION_GRANTED) {
-            ContextCompat.checkSelfPermission(
-                requireContext(),
-                Manifest.permission.ACCESS_FINE_LOCATION
-            ) -> {
-                setUpMap()
-            }
-            else -> {
-                requestPermissionLauncher.launch(
-                    Manifest.permission.ACCESS_FINE_LOCATION
-                )
-            }
-        }
+    private val showUserCurrentLocation: (location: Location) -> Unit = {
+        val userLocation = LatLng(it.latitude, it.longitude)
+        googleMap.addMarker(MarkerOptions().position(userLocation))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 15.0F))
+        googleMap.uiSettings.isZoomControlsEnabled = true
     }
 
-    private fun createLocationRequest(): LocationRequest = LocationRequest.create().apply {
-        interval = 10000
-        fastestInterval = 5000
-        priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setUpListeners()
+
+        if (locationManger.locationPermissionGranted()) {
+            setUpMap()
+        } else {
+            locationManger.requestPermission(requestPermissionLauncher)
+        }
+
     }
 
 
@@ -180,9 +98,6 @@ class MapsFragment : Fragment(R.layout.fragment_maps) {
         Toast.makeText(requireContext(), msg, Toast.LENGTH_LONG).show()
     }
 
-    private fun stopLocationUpdates() {
-        fusedLocationClient.removeLocationUpdates(locationCallback)
-    }
 
     private fun setUpListeners() {
         binding.selectButton.setOnClickListener {
@@ -196,12 +111,7 @@ class MapsFragment : Fragment(R.layout.fragment_maps) {
     }
 
     private fun setUpMap() {
-        fusedLocationClient =
-            LocationServices.getFusedLocationProviderClient(requireActivity())
         mapFragment?.getMapAsync(mapReadyCallback)
     }
 
-    companion object {
-        const val REQUEST_CHECK_SETTINGS = 1546
-    }
 }
