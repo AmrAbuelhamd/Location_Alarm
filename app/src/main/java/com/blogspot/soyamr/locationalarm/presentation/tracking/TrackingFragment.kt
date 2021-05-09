1200f71f153038aad0209f5080a362c40eab41af
Amr Mokhamed <amr.abuelhamd@gmail.com>
2021-05-09 10:35:33 +0700

    Added foreground service to track location of user when the fragment is not visible the application shows notification that it keeps tracking his location, not letting the android platform killing the application.

--- app/src/main/java/com/blogspot/soyamr/locationalarm/presentation/tracking/TrackingFragment.kt
+++ app/src/main/java/com/blogspot/soyamr/locationalarm/presentation/tracking/TrackingFragment.kt
@@ -1,68 +1,208 @@
 package com.blogspot.soyamr.locationalarm.presentation.tracking
 
+import android.Manifest
+import android.content.*
+import android.content.pm.PackageManager
 import android.location.Location
 import android.os.Bundle
+import android.os.IBinder
+import android.util.Log
 import android.view.View
+import androidx.core.app.ActivityCompat
 import androidx.fragment.app.Fragment
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.blogspot.soyamr.locationalarm.R
 import com.blogspot.soyamr.locationalarm.databinding.FragmentTrackingBinding
-import com.blogspot.soyamr.locationalarm.presentation.utils.MyLocation
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
+import com.blogspot.soyamr.locationalarm.presentation.tracking.helper.ForegroundOnlyLocationService
+import com.blogspot.soyamr.locationalarm.presentation.tracking.helper.SharedPreferenceUtil
+import com.blogspot.soyamr.locationalarm.presentation.tracking.helper.toText
+
+private const val TAG = "Heer"
+private const val REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE = 34
+
+class TrackingFragment : Fragment(R.layout.fragment_tracking),
+    SharedPreferences.OnSharedPreferenceChangeListener {
+    private var foregroundOnlyLocationServiceBound = false
+
+    // Provides location updates for while-in-use feature.
+    private var foregroundOnlyLocationService: ForegroundOnlyLocationService? = null
+
+    // Listens for location broadcasts from ForegroundOnlyLocationService.
+    private lateinit var foregroundOnlyBroadcastReceiver: ForegroundOnlyBroadcastReceiver
+
+    private lateinit var sharedPreferences: SharedPreferences
 
 
-class TrackingFragment : Fragment(R.layout.fragment_tracking) {
+    // Monitors connection to the while-in-use service.
+    private val foregroundOnlyServiceConnection = object : ServiceConnection {
+
+        override fun onServiceConnected(name: ComponentName, service: IBinder) {
+            val binder = service as ForegroundOnlyLocationService.LocalBinder
+            foregroundOnlyLocationService = binder.service
+            foregroundOnlyLocationServiceBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName) {
+            foregroundOnlyLocationService = null
+            foregroundOnlyLocationServiceBound = false
+        }
+    }
+
+
+    override fun onStart() {
+        super.onStart()
+
+        updateButtonState(
+            sharedPreferences.getBoolean(SharedPreferenceUtil.KEY_FOREGROUND_ENABLED, false)
+        )
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this)
+
+        val serviceIntent = Intent(requireContext(), ForegroundOnlyLocationService::class.java)
+        requireActivity().bindService(
+            serviceIntent,
+            foregroundOnlyServiceConnection,
+            Context.BIND_AUTO_CREATE
+        )
+    }
+
+    override fun onResume() {
+        super.onResume()
+        LocalBroadcastManager.getInstance(requireContext()).registerReceiver(
+            foregroundOnlyBroadcastReceiver,
+            IntentFilter(
+                ForegroundOnlyLocationService.ACTION_FOREGROUND_ONLY_LOCATION_BROADCAST
+            )
+        )
+    }
+
+    override fun onPause() {
+        LocalBroadcastManager.getInstance(requireContext()).unregisterReceiver(
+            foregroundOnlyBroadcastReceiver
+        )
+        super.onPause()
+    }
+
+    override fun onStop() {
+        if (foregroundOnlyLocationServiceBound) {
+            requireActivity().unbindService(foregroundOnlyServiceConnection)
+            foregroundOnlyLocationServiceBound = false
+        }
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this)
+
+        super.onStop()
+    }
+
+    override fun onSharedPreferenceChanged(sharedPreferences: SharedPreferences, key: String) {
+        // Updates button states if new while in use location is added to SharedPreferences.
+        if (key == SharedPreferenceUtil.KEY_FOREGROUND_ENABLED) {
+            updateButtonState(
+                sharedPreferences.getBoolean(
+                    SharedPreferenceUtil.KEY_FOREGROUND_ENABLED, false
+                )
+            )
+        }
+    }
+
+    // TODO: Step 1.0, Review Permissions: Method checks if permissions approved.
+    private fun foregroundPermissionApproved(): Boolean {
+        return PackageManager.PERMISSION_GRANTED == ActivityCompat.checkSelfPermission(
+            requireContext(),
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    }
+
+
+    // TODO: Step 1.0, Review Permissions: Handles permission result.
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        Log.d(TAG, "onRequestPermissionResult")
+
+        when (requestCode) {
+            REQUEST_FOREGROUND_ONLY_PERMISSIONS_REQUEST_CODE -> when {
+                grantResults.isEmpty() ->
+                    // If user interaction was interrupted, the permission request
+                    // is cancelled and you receive empty arrays.
+                    Log.d(TAG, "User interaction was cancelled.")
+                grantResults[0] == PackageManager.PERMISSION_GRANTED ->
+                    // Permission was granted.
+                    foregroundOnlyLocationService?.subscribeToLocationUpdates()
+                else -> {
+                }
+            }
+        }
+    }
+
+    private fun updateButtonState(trackingLocation: Boolean) {
+        if (trackingLocation) {
+            binding.foregroundOnlyLocationButton.text =
+                getString(R.string.stop_location_updates_button_text)
+        } else {
+            binding.foregroundOnlyLocationButton.text =
+                getString(R.string.start_location_updates_button_text)
+        }
+    }
+
+    private fun logResultsToScreen(output: String) {
+        binding.outputTextView.text = output
+    }
+
+    /**
+     * Receiver for location broadcasts from [ForegroundOnlyLocationService].
+     */
+    private inner class ForegroundOnlyBroadcastReceiver : BroadcastReceiver() {
+
+        override fun onReceive(context: Context, intent: Intent) {
+            val location = intent.getParcelableExtra<Location>(
+                ForegroundOnlyLocationService.EXTRA_LOCATION
+            )
+
+            if (location != null) {
+                logResultsToScreen("Foreground location: ${location.toText()}")
+            }
+        }
+    }
+
     private val binding: FragmentTrackingBinding by viewBinding()
     private val args: TrackingFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
         binding.button.setOnClickListener {
             findNavController().popBackStack()
         }
-        val myLocation = MyLocation()
-        GlobalScope.launch(Dispatchers.Main) {
-            while (estimatedTimeInMinutes > 2) {
-                myLocation.getLocation(requireContext(), locationResult)
-                delay(20000)
+        foregroundOnlyBroadcastReceiver = ForegroundOnlyBroadcastReceiver()
+
+        sharedPreferences =
+            requireActivity().getSharedPreferences(
+                getString(R.string.preference_file_key),
+                Context.MODE_PRIVATE
+            )
+
+//        foregroundOnlyLocationService?.subscribeToLocationUpdates()
+
+        binding.foregroundOnlyLocationButton.setOnClickListener {
+            val enabled = sharedPreferences.getBoolean(
+                SharedPreferenceUtil.KEY_FOREGROUND_ENABLED, false
+            )
+
+            if (enabled) {
+                foregroundOnlyLocationService?.unsubscribeToLocationUpdates()
+            } else {
+                // TODO: Step 1.0, Review Permissions: Checks and requests if needed.
+                if (foregroundPermissionApproved()) {
+                    foregroundOnlyLocationService?.subscribeToLocationUpdates()
+                        ?: Log.d(TAG, "Service Not Bound")
+                } else {
+                    Log.d(TAG, " requestForegroundPermissions()")
+                }
             }
-            //launch notification
-            findNavController().navigate(R.id.action_trackingFragment_to_alarmFragment)
-        }
-    }
-
-    var estimatedTimeInMinutes = 100
-
-    val locationResult = object : MyLocation.LocationResult() {
-
-        override fun gotLocation(location: Location?) {
-
-            val lat = location!!.latitude
-            val lon = location.longitude
-
-            println("$lat --SLocRes-- $lon")
-
-            val location1 = Location("");
-            location1.latitude = lat;
-            location1.longitude = lon;
-
-            val location2 = Location("");
-            location2.latitude = args.lat.toDouble();
-            location2.longitude = args.lng.toDouble();
-
-            val distanceInMeters = location1.distanceTo(location2);
-            val speedIs10MetersPerMinute = 100
-            val estimatedDriveTimeInMinutes = distanceInMeters / speedIs10MetersPerMinute
-            estimatedTimeInMinutes = estimatedDriveTimeInMinutes.toInt();
-            val hours: Int =
-                estimatedDriveTimeInMinutes.toInt() / 60
-            val minutes: Int = estimatedDriveTimeInMinutes.toInt() % 60
-            binding.textView2.text = getString(R.string._5_hours_30_minutes, hours, minutes)
         }
     }
 
